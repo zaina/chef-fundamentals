d3b8445a823cf4390baea435648aec142c7f2612
root <root@workstation.(none)>
2013-12-19 17:42:56 +0000

    Added chef-client, cron and logrotate cookbooks

--- roles/base.rb
+++ roles/base.rb
@@ -1,6 +1,6 @@
 name "base"
 description "Base server role"
-def_run_list = ["recipe[motd]", "recipe[users]" ]
+def_run_list = ["recipe[chef-client::delete_validation]", "recipe[motd]", "recipe[users]" ]
 env_run_lists({
 	"_default" => def_run_list,
 	"dev" => def_run_list,
